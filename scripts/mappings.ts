f7ab0df4d1afde893c8cd9938336f5ca8189fc6e
shane F <shane.frykholm@moovweb.com>
2013-11-05 01:20:01 -0800

    basic mobile xkcd

--- scripts/mappings.ts
+++ scripts/mappings.ts
@@ -18,7 +18,7 @@ match($status) {
     log("--> STATUS: 200")
 
     match($path) {
-      with(/^\/$|^\/\?/) {
+      with(/^\/$|^\/\?|^\/\d+/) {
         log("--> Importing pages/home.ts in mappings.ts")
         @import pages/home.ts
       }
--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,1 +1,13 @@
-# Place holder/example file
+$("/html/head") {
+	inject("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">")
+}
+
+$("/html/body"){
+	add_class("mw_home")
+	move_here("//div[@id='comic']","top")
+	move_here("//ul[contains(@class,'comicNav')][1]","bottom")
+	remove(".//a[contains(@href,'random')]")
+	remove(".//div[@id='topContainer']")
+	remove(".//div[@id='bottom']")
+	remove(".//div[@id='middleContainer']")
+}
